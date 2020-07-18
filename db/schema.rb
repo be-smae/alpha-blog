f308b06205045ad9fe941592bb75d5a086f7b473
Beau <be@bestmoa.dev>
2020-07-18 02:19:18 +0000

    Add routes, controllers, show for articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_211556) do
+ActiveRecord::Schema.define(version: 2020_07_08_215012) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
