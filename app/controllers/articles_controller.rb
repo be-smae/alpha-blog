dfadcac8ea052da34deb276f872a53451d724312
Beau <be@bestmoa.dev>
2020-07-27 17:57:00 +0000

    Add edit and update to articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,38 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   
+  def new
+    @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+      # if @article.save
+      #   flash[:notice] = "Article was edited successfully."
+      #   redirect_to @article
+      # else
+      #   render 'edit'
+      # end
+  end
+  
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
 end
